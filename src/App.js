7688227168e0a3f689285956428ea118135a129b
Akshay Kumar S <akshay.s@experionglobal.local>
2023-01-24 10:39:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+
+import Home from './Components/LandingPage/home';
+import Userhome from './Components/LandingPage/UserHome';
+import Login from './Components/Login/Login';
+import Forgot from './Components/Login/ForgotPassword';
+import Register from './Components/Registration/register';
+import Product from './Components/pages/Products';
+import ProductDetail from './Components/pages/DetailViewProduct';
+import Cart from './Components/pages/Cart';
+import Check from './Components/Checkout/Checkout';
+
+const router = createBrowserRouter([
+  {
+    path: '/home',
+    element: <Home />
+  },
+  {
+    path: '/regis',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/userhome',
+    element: <Userhome />,
+  },
+  {
+    path: '/product',
+    element: <Product/>
+  },
+  {
+    path: '/productdetail/:id',
+    element:<ProductDetail />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/checkout/:id',
+    element: <Check />
+  },
+  {
+    path: '/forgot',
+    element: <Forgot />
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <RouterProvider router={router} />
+    </>
+  )
 }
 
 export default App;
